0353a24662522500cbb3728e900f8efd0c942b34
KRader627 <klr020@bucknell.edu>
2017-02-22 16:11:24 -0500

    Added in Launch pad and rocket behavior update

--- Assets/Scripts/RocketBehavior.cs
+++ Assets/Scripts/RocketBehavior.cs
@@ -7,75 +7,80 @@ public class RocketBehavior : MonoBehaviour {
 
     public ParticleSystem particleSyst = null;
     public Transform cam = null;
-	int numFuelPods = 3;
+
+    int numFuelPods = 3;
 	int fuelPos = 0;
 	float moveSpeed = 5.0f;
 
 	public static bool launch = false;
 
+	Vector3 prevRocketDirectionVector = new Vector3 (0, 0, 0);
+	Vector3 prevDirection = new Vector3 (0, 0, 0);
+
 	Vector3 prevPos = new Vector3 (0, 0, 0);
-	Vector3 prevPrevPos = new Vector3 (0, 0, 0);
-	float rocketX = 0;
+    float rocketX = 0;
 	float rocketY = 0;
-	float rocketZ = 0;
-	float initialX = 0;
+    float rocketZ = 0;
+
+    float initialX = 0;
 	float initialY = 0;
 
-	float velocity = 100;//RocketState.fuel;
+	float velocity = 500;//RocketState.fuel;
 
-	//float angleRad = 60 * ((float)Math.PI) / 180;//RocketState.angle * ((float)Math.PI) / 180;
-	//float currAngle =  60 * ((float)Math.PI) / 180;//RocketState.angle * ((float)Math.PI) / 180;
-	float angleRad = 0f;
-	float currAngle = 0f;
+	float angleRad = RocketState.angle * ((float)Math.PI) / 180;
+	float currAngle = RocketState.angle * ((float)Math.PI) / 180;
 		
+	float gameTime = 0f;
 	float time = 0f;
 	float timeMult = 0f;
 
-    //Win/lose conditions:
-    public int min_height = 5900;
-    public int max_height = 9900;
+	Boolean turning = true;
+
+    //Win/Lose conditions:
+    public int min_height = 6000;
+    public int max_height = 10000;
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		// Listens for lift off key
-		if (Input.GetKeyDown(KeyCode.Return) && launch == false) {
-			
+        // Listens for lift off key
 
-			//Debug.Log("Lift off!!!");
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+        rocketX = transform.position.x;
+        rocketY = transform.position.y;
+        rocketZ = transform.position.z;
+
+        if (Input.GetKeyDown(KeyCode.Return) && launch == false) {
+
+			Debug.Log("Lift off!!!");
 			launch = true;
-			particleSyst.Play();
+            particleSyst.Play();
             ScreenChanges.launch_sounds();
-			//particleSyst2.Play ();
-			initialX = transform.position.x;
-			initialY = transform.position.y;
-			rocketX = transform.position.x;
-			rocketY = transform.position.y;
-			rocketZ = transform.position.z;
 
-			velocity = RocketState.fuel * 5;
-			angleRad = (180-RocketState.angle) * ((float)Math.PI) / 180;
-			currAngle = (180-RocketState.angle) * ((float)Math.PI) / 180;
-			//Debug.Log (RocketState.angle.ToString ());
-			//Debug.Log (RocketState.fuel.ToString ());
-
-			//Vector3 yTarget = Camera.main.transform.forward - (transform.forward * Vector3.Dot(Camera.transform.forward, transform.forward));
-			// Find the needed rotation to rotate y to y-target
-			//Quaternion desiredRotation = Quaternion.LookRotation(transform.forward, yTarget);
-			// Apply that rotation
-			//transform.rotation = desiredRotation;
+            velocity = RocketState.fuel * 5;
+			angleRad = RocketState.angle * ((float)Math.PI) / 180;
+			currAngle = RocketState.angle * ((float)Math.PI) / 180;
 		}
 
 		// Handles moving rocket
-		if (launch == true) {   
-            //Switch the GUI into Launch mode
+		if (launch == true) {
+            //Switch the GUI mode:
             GUISwitch.launch_mode();
             // update position of rocket
-            float old_y_pos = transform.position.y;
-            Vector3 move = trajectory();
-			transform.position = move;
-            float new_y_pos = transform.position.y;
+			Vector3 dVector = (transform.position - prevPos).normalized;
+			Vector3 RocketDirectionVector = (new Vector3((float) Math.Cos(angleRad), (float) Math.Sin(angleRad), 0).normalized)*velocity;
+			Quaternion vQ = Quaternion.LookRotation (Vector3.forward, RocketDirectionVector);
+			Vector3 toVec = vQ.ToEulerAngles();
 
-            rotation();
+			Vector3 move = launchSequence();
+
+			if (Math.Pow(gameTime,3) < velocity) {
+				move = launchSequence();
+			} else if (Quaternion.Angle(transform.rotation,vQ) > 5f) {
+				move = launchToPathTransition (RocketDirectionVector,vQ);
+			} else {
+				move = trajectory(vQ);
+			}
 
 			// update time
 			time += Time.deltaTime * timeMult;
@@ -83,13 +88,12 @@ public class RocketBehavior : MonoBehaviour {
 				timeMult += 0.01f;
 			}
 			// update position variables
-			prevPrevPos = prevPos;
 			prevPos = transform.position;
+            float old_y_pos = transform.position.y;
+            transform.position = move;
+            float new_y_pos = transform.position.y;
 
-            //Win/Lose Condition handling:
-            //If the rocket is going vertically downward...
-            print(new_y_pos - old_y_pos);
-            if (new_y_pos - old_y_pos <= -10 )
+            if (new_y_pos - old_y_pos <= -10)
             {
                 //If it's too low
                 if (new_y_pos < min_height)
@@ -113,40 +117,19 @@ public class RocketBehavior : MonoBehaviour {
                     ScreenChanges.staticSpecificScene("Win_Screen");
                 }
             }
-		}
+
+        }
 
 		// Handles dropping fuel pods
 		if ( (Input.GetKeyDown(KeyCode.Space)) && (fuelPos < numFuelPods) ) {
 			Transform pod = transform.GetChild(0);
-			//Vector3 childPos = pod.position;
-			//Vector3 breakAwayAngle = new Vector3(childPos.x,0,childPos.z);
 			pod.parent = null;
 			pod.gameObject.AddComponent<Rigidbody>();
-			//pod.gameObject.GetComponent<Rigidbody>().velocity = breakAwayAngle;
 			fuelPos += 1;
 			changeAngle (3);
 		}
-
 	}
-
-	void rotation() {
-		/* Rotates the rocket such that it initially starts in the upward
-		 * direction. Then it slowly turns towards the user's specified
-		 * angle. Finally, it is angled along the tangent line of its path. */
-		Vector3 dVector = (transform.position - prevPos).normalized;
-		Quaternion vQ = Quaternion.LookRotation (Vector3.forward, dVector);
-		Vector3 toVec = vQ.ToEulerAngles();
-		if (time < 1) {
-			return;
-		} else if (Vector3.Distance(transform.eulerAngles, toVec) > 0.01f) {
-			transform.rotation = Quaternion.Lerp(transform.rotation, vQ, 0.5f*Time.deltaTime);
-            GameObject.Find("HUD").transform.forward = cam.forward;
-            //transform.Rotate(0,0,5);
-        } else {
-			transform.rotation = vQ;
-		}
-	}
-
+		
 	void changeAngle(float amount) {
 		// change angle if fuel drop
 		velocity = (float) (Math.Sqrt ((4.9 * rocketX) / (Math.Sin (angleRad) * Math.Cos (angleRad)))); //gets curr velocity
@@ -156,7 +139,32 @@ public class RocketBehavior : MonoBehaviour {
 		angleRad += amount * ((float)Math.PI) / 180; // makes new angle
 	}
 
-	Vector3 trajectory() {
+	Vector3 launchSequence() {
+		/* This function moves the rocket in an upwards direction
+		 * at a cubicaly increasing rate
+		 */
+		gameTime += Time.deltaTime;
+		rocketY += gameTime*gameTime*gameTime;
+		return new Vector3 (rocketX, rocketY, rocketZ);
+	}
+
+	Vector3 launchToPathTransition(Vector3 RocketDirection, Quaternion vQ) {
+		/* This function moves and rotates the rocket so that 
+		 * it can smoothly transition to follow the trajectory
+		 * given by the user input
+		 */
+		// rotate the rocket
+		transform.rotation = Quaternion.Lerp(transform.rotation, vQ, 0.3f*Time.deltaTime);
+		// move the position of the rocket
+		rocketX += RocketDirection.x;
+		rocketY += RocketDirection.y;
+        GameObject.Find("HUD").transform.forward = cam.forward;
+        return new Vector3 (rocketX, rocketY, rocketZ);
+	}
+
+	Vector3 trajectory(Quaternion vQ) {
+		turning = false;
+		transform.rotation = vQ;
 		rocketX = initialX + velocity * ((float)Math.Cos(angleRad)) * time;
 		rocketY = (float) (initialY + velocity * Math.Sin(angleRad) * time - 0.5 * 9.8 * time * time);
 		return new Vector3(rocketX,rocketY,rocketZ);
