4e8ed7db19ad3c8b117c0a35a4d0f79a93bc2d7b
Keyi Zhang <Kuree@users.noreply.github.com>
2017-02-27 14:05:55 -0500

    added color change code.

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+    // Use this for initialization
+    int counter = 0;
+    Color[] colorList = { Color.red, Color.white, Color.yellow, Color.blue};
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            foreach(var obj in GetComponentsInChildren(typeof(Renderer)))
+            {
+                var render = obj.GetComponent<Renderer>();
+                foreach(var material in render.materials)
+                {
+                    if(material.name.Contains("FrontCol") || material.name.Contains("_Gray_") || material.name.Contains("Material"))
+                        material.SetColor("_Color", colorList[counter]);
+                    
+                }
+            }
+            counter = (counter + 1) % colorList.Length;
+
+        }
+	}
+}
