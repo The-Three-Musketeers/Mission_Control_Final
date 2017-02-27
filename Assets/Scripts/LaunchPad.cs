30b9f14d29674567c46ee57d1b1beb42462ab4d4
Keyi Zhang <Kuree@users.noreply.github.com>
2017-02-27 13:42:00 -0500

    Update LaunchPad.cs

--- Assets/Scripts/LaunchPad.cs
+++ Assets/Scripts/LaunchPad.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,12 +9,20 @@ public class LaunchPad : MonoBehaviour {
 
     public static bool animationDone = false;
     bool button_hit_flag = false;
+    public float animationSpeed;
+    public Animation _animation;
+
+    void Start () {
+        _animation = GetComponent<Animation>();
+        animationSpeed = 5f;
+    }
 
 	// Update is called once per frame
-	void Update () {
+    void Update () {
         Animation animation = GetComponent<Animation>();
         if (Input.GetKeyDown(KeyCode.Return)) {
-            animation.Play();
+	    _animation["Expand"].speed = animationSpeed;
+            _animation.Play();
             RocketBehavior.launch = true;
             button_hit_flag = true;
         }
