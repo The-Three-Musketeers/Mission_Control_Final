a6384559104677a59333dcdd89aeee2c43a6007e
Michael Hammer <mph009@bucknell.edu>
2017-03-01 15:05:15 -0500

    debugged rocket trajectory

--- Assets/Scripts/RocketBehavior.cs
+++ Assets/Scripts/RocketBehavior.cs
@@ -25,10 +25,9 @@ public class RocketBehavior : MonoBehaviour {
     float initialX = 0;
 	float initialY = 0;
 
-	float velocity = 500;//RocketState.fuel;
+	float velocity = RocketState.fuel;
 
 	float angleRad = RocketState.angle * ((float)Math.PI) / 180;
-	float currAngle = RocketState.angle * ((float)Math.PI) / 180;
 		
 	float gameTime = 0f;
 	float time = 0f;
@@ -54,20 +53,21 @@ public class RocketBehavior : MonoBehaviour {
 
 			Debug.Log("Lift off!!!");
 			launch = true;
-            particleSyst.Play();
-            ScreenChanges.launch_sounds();
+            //particleSyst.Play();
+            //ScreenChanges.launch_sounds();
 
-            velocity = RocketState.fuel * 5;
+            velocity = RocketState.fuel;
 			angleRad = (180 - RocketState.angle) * ((float)Math.PI) / 180;
-			currAngle = (180 - RocketState.angle) * ((float)Math.PI) / 180;
 		}
+
+		Vector3 dVector = (transform.position - prevPos).normalized;
 
 		// Handles moving rocket
 		if (launch == true) {
             //Switch the GUI mode:
             GUISwitch.launch_mode();
             // update position of rocket
-			Vector3 dVector = (transform.position - prevPos).normalized;
+			dVector = (transform.position - prevPos).normalized;
 			Vector3 RocketDirectionVector = (new Vector3((float) Math.Cos(angleRad), (float) Math.Sin(angleRad), 0).normalized)*velocity;
 			Quaternion vQ = Quaternion.LookRotation (Vector3.forward, RocketDirectionVector);
 			Vector3 toVec = vQ.ToEulerAngles();
@@ -76,17 +76,19 @@ public class RocketBehavior : MonoBehaviour {
 
 			if (Math.Pow(gameTime,3) < velocity) {
 				move = launchSequence();
-			} else if (Quaternion.Angle(transform.rotation,vQ) > 5f) {
+			} else if (turning && Quaternion.Angle(transform.rotation,vQ) > 5f) {
 				move = launchToPathTransition (RocketDirectionVector,vQ);
 			} else {
-				move = trajectory(vQ);
+				move = trajectory(dVector);
 			}
 
+
+
 			// update time
-			time += Time.deltaTime * timeMult;
+			/*time += Time.deltaTime * timeMult;
 			if (timeMult < 4) {
 				timeMult += 0.01f;
-			}
+			}*/
 			// update position variables
 			prevPos = transform.position;
             float old_y_pos = transform.position.y;
@@ -122,56 +124,64 @@ public class RocketBehavior : MonoBehaviour {
 
 		// Handles dropping fuel pods
 		if ( (Input.GetKeyDown(KeyCode.Space)) && (fuelPos < numFuelPods) ) {
-			Transform pod = transform.GetChild(0);
-			pod.parent = null;
-			pod.gameObject.AddComponent<Rigidbody>();
-			fuelPos += 1;
-			changeAngle (3);
+			//Transform pod = transform.GetChild(0);
+			//pod.parent = null;
+			//pod.gameObject.AddComponent<Rigidbody>();
+			//fuelPos += 1;
+			changeAngle (20,dVector);
 		}
 	}
-		
-	void changeAngle(float amount) {
+
+	void changeAngle(float amount, Vector3 dVector) {
 		// change angle if fuel drop
 		velocity = (float) (Math.Sqrt ((4.9 * rocketX) / (Math.Sin (angleRad) * Math.Cos (angleRad)))); //gets curr velocity
+		angleRad = (float) (Math.PI - Math.Cos(dVector.x)); //sets angleRad to current angle
 		initialX = rocketX;
 		initialY = rocketY;
 		time = 0; //resets equation
-		angleRad += amount * ((float)Math.PI) / 180; // makes new angle
+		Debug.Log((angleRad * 180 / Math.PI).ToString());
+		angleRad -= amount * ((float)Math.PI) / 180; // makes new angle
+		Debug.Log((angleRad * 180 / Math.PI).ToString());
 	}
 
+	/* This function moves the rocket in an upwards direction
+	 * at a cubicaly increasing rate */
 	Vector3 launchSequence() {
-		/* This function moves the rocket in an upwards direction
-		 * at a cubicaly increasing rate
-		 */
+
 		gameTime += Time.deltaTime;
 		rocketY += gameTime*gameTime*gameTime;
 		return new Vector3 (rocketX, rocketY, rocketZ);
 	}
 
+	/* This function moves and rotates the rocket so that 
+	 * it can smoothly transition to follow the trajectory
+	 * given by the user input */
 	Vector3 launchToPathTransition(Vector3 RocketDirection, Quaternion vQ) {
-		/* This function moves and rotates the rocket so that 
-		 * it can smoothly transition to follow the trajectory
-		 * given by the user input
-		 */
 		// rotate the rocket
-		transform.rotation = Quaternion.Lerp(transform.rotation, vQ, 0.3f*Time.deltaTime);
+		transform.rotation = Quaternion.Slerp(transform.rotation, vQ, 0.5f*Time.deltaTime);
+		GameObject.Find("HUD").transform.forward = cam.forward;
 		// move the position of the rocket
 		rocketX += RocketDirection.x;
 		rocketY += RocketDirection.y;
-        GameObject.Find("HUD").transform.forward = cam.forward;
-        return new Vector3 (rocketX, rocketY, rocketZ);
+		initialX = rocketX;
+		initialY = rocketY;
+		return new Vector3 (rocketX, rocketY, rocketZ);
 	}
 
-	Vector3 trajectory(Quaternion vQ) {
+	Vector3 trajectory(Vector3 dVec) {
 		turning = false;
-		transform.rotation = vQ;
+		Quaternion vQ = Quaternion.LookRotation (Vector3.forward, dVec);
+		transform.rotation = Quaternion.Slerp(transform.rotation, vQ, 0.5f*Time.deltaTime);
+		GameObject.Find("HUD").transform.forward = cam.forward;
 		rocketX = initialX + velocity * ((float)Math.Cos(angleRad)) * time;
 		rocketY = (float) (initialY + velocity * Math.Sin(angleRad) * time - 0.5 * 9.8 * time * time);
+		time += Time.deltaTime;
 		return new Vector3(rocketX,rocketY,rocketZ);
 	}
 
 	// Use this for initialization
 	void Start () {
+
 
 	}
 }
