5549f31970d945df95fe73db286bf5adf93d6972
KRader627 <klr020@bucknell.edu>
2017-03-05 16:48:54 -0500

    Minor UI Improvements and New Feature: Global Inactvity Timer, fully tested

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 //This class keeps track of all game variables, persisting across
@@ -11,7 +12,11 @@ public class GameState : MonoBehaviour {
 	public static bool fuel_selected = false;
 	public static bool angle_selected = false;
 
-	public string get_mission() {
+    //Handling for inactivity timer
+    int timer;
+    public static AudioSource audio2;
+
+    public string get_mission() {
 		return mission;
 	}
 
@@ -42,5 +47,36 @@ public class GameState : MonoBehaviour {
 	public static void unselect_angle() {
 		angle_selected = false;
 	}
+
+    private void Start() {
+        if (audio2 == null) { audio2 = GameObject.Find("Audio2").GetComponent<AudioSource>(); DontDestroyOnLoad(audio2); }
+    }
+
+    //The Update function is for the inactivity timer only
+    private void Update() {
+        //If the user presses a key, reset the timer
+        if (Input.anyKeyDown) {
+            timer = 0;
+        }
+        timer += 1;
+        //If the timer reaches 9000 (five minutes at 30fps) with no input, reset everything
+        //and go to the Title Screen
+        if (timer >= 9000) {
+            LaunchPad.reset();
+            Manual_Click.reset();
+            //Rare case, if reset on gameplay screen, make sure to change the music
+            if (SceneManager.GetActiveScene().name == "Gameplay") {
+                ScreenChanges.changeMusic();
+            }
+            //Rare case, if rocket is launching and it resets, turn off the launch sounds
+            if (audio2.isPlaying == true) {
+                ScreenChanges.launch_sounds();
+            }
+            Selector.reset();
+            Skybox.reset();
+            ScreenChanges.staticSpecificScene("TitleScreen");
+            Camera.reset();
+        }
+    }
 
 }
